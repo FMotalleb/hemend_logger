ca98212b7e75c414598f0a8fe49bc6d14e649502
FMotalleb <fmotalleb@gmail.com>
2023-05-21 14:00:21 +0330

    [Example]

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,4 @@
-import 'package:hemend_logger/src/hemend_logger.dart';
+import 'package:hemend_logger/hemend_logger.dart';
 import 'package:logging/logging.dart';
 
 void main() {
@@ -11,5 +11,29 @@ void main() {
     ..config('this is config with level value:${Level.CONFIG.value}')
     ..info('this is info with level value:${Level.INFO.value}')
     ..warning('this is warning with level value:${Level.WARNING.value}')
+    ..severe('this is severe with level value:${Level.SEVERE.value}')
     ..shout('this is shout with level value:${Level.SHOUT.value}');
+  ExampleClass().test();
+}
+
+class ExampleClass with LogableObject {
+  @override
+
+  /// You should not use toString() here
+  /// its only use-case is in debug mode
+  /// under development environment this
+  /// value is not class name
+  // ignore: no_runtimetype_tostring
+  String get loggerName => runtimeType.toString();
+  void test() {
+    finest('this is finest with level value:${Level.FINEST.value}');
+    finer('this is finer with level value:${Level.FINER.value}');
+    fine('this is fine with level value:${Level.FINE.value}');
+    config('this is config with level value:${Level.CONFIG.value}');
+    info('this is info with level value:${Level.INFO.value}');
+    warning('this is warning with level value:${Level.WARNING.value}');
+    shout('this is shout with level value:${Level.SHOUT.value}');
+    // you can access logger itself by using this property
+    config(logger.name);
+  }
 }
