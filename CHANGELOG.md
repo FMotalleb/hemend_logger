8a3db6d0d69850b73210f7c0465550ae1e4c9cc6
FMotalleb <fmotalleb@gmail.com>
2023-05-23 13:36:03 +0330

    [Fix]<Package>: exporting contracts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.3+1
+
+* fix on exporting the contracts
+
 ## 0.1.2+1
 
 * minor documentation and example changes
--- lib/hemend_logger.dart
+++ lib/hemend_logger.dart
@@ -3,6 +3,8 @@ library hemend_logger;
 
 export 'src/ansi_logger/ansi_colors.dart';
 export 'src/ansi_logger/ansi_logger.dart';
+export 'src/contracts/logger/logger.dart';
+export 'src/contracts/typedefs.dart';
 export 'src/helper/diagnosable_object.dart';
 export 'src/hemend_logger.dart';
 export 'src/log_decorators/ansi_message_color_decorator.dart';
